e7d03cd27fce689b9ec472519a81a6bcd1f343ea
robtaylor <robtaylor@users.noreply.github.com>
2026-05-12 11:40:33 +0000

    deploy: 499269b61943586a54174a47d98ac4c63ff51dd3

--- api/trait.impl/core/str/traits/trait.FromStr.js
+++
@@ -1,9 +0,0 @@
-(function() {
-    const implementors = Object.fromEntries([["jacquard",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.95.0/core/str/traits/trait.FromStr.html\" title=\"trait core::str::traits::FromStr\">FromStr</a> for <a class=\"enum\" href=\"jacquard/sdf_parser/enum.SdfCorner.html\" title=\"enum jacquard::sdf_parser::SdfCorner\">SdfCorner</a>",0]]]]);
-    if (window.register_implementors) {
-        window.register_implementors(implementors);
-    } else {
-        window.pending_implementors = implementors;
-    }
-})()
-//{"start":59,"fragment_lengths":[308]}
