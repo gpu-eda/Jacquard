d6fa941b62894b65a0db8cd3fa3967d55fdaf4c1
robtaylor <robtaylor@users.noreply.github.com>
2026-05-25 20:21:42 +0000

    deploy: 52a497ae55f0f8bf72187a59fd4e5f152f179744

--- api/jacquard/sim/sidebar-items.js
+++ api/jacquard/sim/sidebar-items.js
@@ -1,1 +1,1 @@
-window.SIDEBAR_ITEMS = {"mod":["cpu_reference","input_stim","models","setup","sram_dump","sram_preload","timing_ir_loader","trace_signals","vcd_io"]};
+window.SIDEBAR_ITEMS = {"mod":["cpu_reference","input_stim","models","run_params","setup","sram_dump","sram_preload","timing_ir_loader","trace_signals","vcd_io"]};
