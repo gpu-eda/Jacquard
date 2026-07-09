20f47c8628643e1138159469aab5644065e5e836
robtaylor <robtaylor@users.noreply.github.com>
2026-07-09 14:47:49 +0000

    deploy: 346b0169e6cbd5f0941d79f4bf00a0ada371f429

--- api/trait.impl/core/convert/trait.TryFrom.js
+++ api/trait.impl/core/convert/trait.TryFrom.js
@@ -1,5 +1,5 @@
 (function() {
-    const implementors = Object.fromEntries([["jacquard",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.96.1/core/convert/trait.TryFrom.html\" title=\"trait core::convert::TryFrom\">TryFrom</a>&lt;<a class=\"primitive\" href=\"https://doc.rust-lang.org/1.96.1/std/primitive.u32.html\">u32</a>&gt; for <a class=\"enum\" href=\"jacquard/event_buffer/enum.EventType.html\" title=\"enum jacquard::event_buffer::EventType\">EventType</a>",0]]]]);
+    const implementors = Object.fromEntries([["jacquard",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.97.0/core/convert/trait.TryFrom.html\" title=\"trait core::convert::TryFrom\">TryFrom</a>&lt;<a class=\"primitive\" href=\"https://doc.rust-lang.org/1.97.0/std/primitive.u32.html\">u32</a>&gt; for <a class=\"enum\" href=\"jacquard/event_buffer/enum.EventType.html\" title=\"enum jacquard::event_buffer::EventType\">EventType</a>",0]]]]);
     if (window.register_implementors) {
         window.register_implementors(implementors);
     } else {
--- api/trait.impl/core/ops/drop/trait.Drop.js
+++ api/trait.impl/core/ops/drop/trait.Drop.js
@@ -1,5 +1,5 @@
 (function() {
-    const implementors = Object.fromEntries([["jacquard",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.96.1/core/ops/drop/trait.Drop.html\" title=\"trait core::ops::drop::Drop\">Drop</a> for <a class=\"struct\" href=\"jacquard/testbench/struct.CppSpiFlash.html\" title=\"struct jacquard::testbench::CppSpiFlash\">CppSpiFlash</a>",0]]]]);
+    const implementors = Object.fromEntries([["jacquard",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.97.0/core/ops/drop/trait.Drop.html\" title=\"trait core::ops::drop::Drop\">Drop</a> for <a class=\"struct\" href=\"jacquard/testbench/struct.CppSpiFlash.html\" title=\"struct jacquard::testbench::CppSpiFlash\">CppSpiFlash</a>",0]]]]);
     if (window.register_implementors) {
         window.register_implementors(implementors);
     } else {
